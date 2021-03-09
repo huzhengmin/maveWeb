7b5e91b24cef186ee87d5c96389b961742451a3f
hu-zhengmin <562768552@qq.com>
2021-03-09 16:31:28 +0800

    myUtil

---
+++ src/main/java/com/gaotai/util/MyUtil.java
@@ -0,0 +1,21 @@
+package com.gaotai.util;
+
+import java.util.Date;
+
+/**
+ * @author： hzm
+ * @date： 2021/3/9 16:28
+ * @description： TODO
+ * @modifiedBy：
+ * @version:
+ */
+public class MyUtil {
+    public String getStr(String str)
+    {
+        return  str;
+    }
+    public String getStr(Date date)
+    {
+        return date.toString();
+    }
+}
